a75e3fae58798a47059b7d81b0b765129526b6e4
Akshat Jain <akshatmeeta@gmail.com>
2021-01-15 20:27:15 +0530

    Update urls.py

--- cs_project/urls.py
+++ cs_project/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include(urls), name='accounts'),
     path('',include('pp_index.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
