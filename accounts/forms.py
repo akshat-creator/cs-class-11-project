7681238d9a96d8c299f4575b2c982cced28bb6c5
akshat-creator <akshatjain@gmail.com>
2021-01-15 18:39:43 +0530

    stuff

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.db import transaction
 from .models import User,Customer,ShopKeeper
+from django.contrib.auth.models import Group
 
 class CustomerSignUpForm(UserCreationForm):
     first_name = forms.CharField(required=True)
@@ -36,11 +37,14 @@ class ShopkeeperSignUpForm(UserCreationForm):
     def save(self):
         user = super().save(commit=False)
         user.is_shopkeeper = True
+        user.is_staff = True
         user.first_name = self.cleaned_data.get('first_name')
         user.last_name = self.cleaned_data.get('last_name')
         user.save()
-        shopkeeper = Employee.objects.create(user=user)
+        shopkeeper = ShopKeeper.objects.create(user=user)
         shopkeeper.phone_number=self.cleaned_data.get('phone_number')
         shopkeeper.designation=self.cleaned_data.get('designation')
         shopkeeper.save()
+        Shop_keepers = Group.objects.get(name='Shop_keepers')
+        Shop_keepers.user_set.add(user)
         return user
